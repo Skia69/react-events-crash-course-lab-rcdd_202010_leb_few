627465b4973793e3e8bca656edb18cf94c57b13f
Mhamad Ibrahim <skiastep@gmail.com>
2021-01-17 17:14:07 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -36,6 +36,9 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+          onMouseMove={this.handleMouseMove}
+        onClick={() => {toggleCycling()}}
+        onKeyDown={this.handleKeyDown}
         width='900'
         height='600'
         tabIndex="0">
